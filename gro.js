a2b4eb0247a8bb41d9e92a9d15ba68b4dd753c9b
GroEmilie <95849204+GroEmilie@users.noreply.github.com>
2022-04-20 19:58:05 +0200

    nyhedsbrev

--- gro.js
+++ gro.js
@@ -1,21 +1,28 @@
-console.log ("javascript running");
-// start carrousel
-let('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: false
- });
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
 
 
- // move next carousel
- let('.moveNextCarousel').click(function(e){
-    e.preventDefault();
-    e.stopPropagation();
-    $('.carousel').carousel('next');
- });
-
- // move prev carousel
- let('.movePrevCarousel').click(function(e){
-    e.preventDefault();
-    e.stopPropagation();
-    $('.carousel').carousel('prev');
- });
